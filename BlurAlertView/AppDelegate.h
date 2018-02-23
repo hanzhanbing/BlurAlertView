a23cee03953b4c1965cb9959fb57ffe6272e1597
hanzhanbing <1655661337@qq.com>
2018-02-23 14:10:07 +0800

    Initial Commit

---
+++ BlurAlertView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlurAlertView
+//
+//  Created by Jason on 2018/2/23.
+//  Copyright © 2018年 hzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlurAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlurAlertView
+//
+//  Created by Jason on 2018/2/23.
+//  Copyright © 2018年 hzb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
